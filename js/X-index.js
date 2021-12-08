bf73667eb3881573ec86ed8f57da03d9213b01b9
谢晓雪 <1841762072@qq.com>
2021-12-08 17:14:47 +0800

    '主页2版'

---
+++ js/X-index.js
@@ -0,0 +1,6 @@
+let download_ = document.getElementById('X-free-download');
+let close_ = document.getElementById('X-free-download-close');
+
+close_.onclick = function (){
+    download_.style.display='none';
+}
